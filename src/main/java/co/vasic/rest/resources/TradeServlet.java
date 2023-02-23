f01a6d4ac36b5832304b3fd2affcb9d6e042a3e7
anton95i <anton.steiner@hotmail.com>
2023-02-23 21:02:24 +0100

    2102

--- src/main/java/co/vasic/rest/resources/TradeServlet.java
+++ src/main/java/co/vasic/rest/resources/TradeServlet.java
@@ -175,7 +175,8 @@ public class TradeServlet extends HttpServlet {
 
         Matcher m = pattern.matcher(request.getPath());
         if (m.matches()) {
-            String id = m.group(1);
+            //String id = m.group(1);
+            String id = request.getParams().get("id");
 
             Trade trade = (Trade) tradeService.getTrade(id);
 
